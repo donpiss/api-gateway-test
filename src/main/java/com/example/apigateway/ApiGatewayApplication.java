911ce1373bcbde80b5a3b926fedca1f6631bb72a
Donny <donnypissu>
2017-12-04 18:17:36 +0700

    first commit

---
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.example.apigateway.config.SimpleFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+}
---
+++ src/main/java/com/example/apigateway/config/SimpleFilter.java
@@ -0,0 +1,42 @@
+package com.example.apigateway.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class SimpleFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+
+		log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+		log.debug(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+		log.warn(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+		return null;
+	}
+
+}
